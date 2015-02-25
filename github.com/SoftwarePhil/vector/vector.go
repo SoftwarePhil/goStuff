2a6076ee43a94ddc9ea8b382c67252011c91a3d9
SoftwarePhil <plantlife26@gmail.com>
2015-02-24 22:27:45 -0500

    v5

--- github.com/SoftwarePhil/vector/vector.go
+++ github.com/SoftwarePhil/vector/vector.go
@@ -7,9 +7,20 @@ import "strconv"
 func main(){
 		vec := Vector{3,4}
 		vec3D := Vector3D{vec,5}
-		//vec3D2 := Vector3D{Vector{1,1},1}
+		vec3D2 := Vector3D{Vector{1,1},1}
 		fmt.Println("The unit vector of", vec3D.print(), "is", vec3D.unitVector().print())
-		fmt.Println("Vector", vec.print(), "times scalar 4 is", vec.scalarMult(4).print())
+		fmt.Println("The dot product of", vec3D.print(), "and", vec3D2.print(), "is", vec3D.dotProduct(vec3D2))
+		fmt.Println("Vector", vec3D.print(), "times scalar 4 is", vec3D.scalarMult(4).print())
+		fmt.Println("Vector", vec3D.print(), "cross", vec3D2.print(), "equals", vec3D.crossProduct(vec3D2).print())
+		
+		a := Vector3D{Vector{0,3},1}
+		b := Vector3D{Vector{-3,7},8}
+		fmt.Println("Scalar Mult", a.scalarMult(a.dotProduct(b)).print())
+		fmt.Println("cross product a cross a", a.crossProduct(a).print())
+		
+		c := Vector3D{Vector{1,1},1}
+		d := Vector3D{Vector{1,-1},-1}
+		fmt.Println("Angle between", c.print(), "and", d.print(), "is", c.angle(d))
 }
 
 type Vector struct{
@@ -55,20 +66,22 @@ func (vec Vector3D) print() string{
 }
 
 func(vec Vector) unitVector() Vector{
-	l := vec.length()
-	return Vector{(vec.x/l),(vec.y/l)}
+		l := vec.length()
+	
+		return Vector{(vec.x/l),(vec.y/l)}
 }
 
 func(vec Vector3D) unitVector() Vector3D{
-	l := vec.length()
-	return Vector3D{Vector{(vec.x/l), (vec.y/l)}, (vec.z/l)}
+		l := vec.length()
+	
+		return Vector3D{Vector{(vec.x/l), (vec.y/l)}, (vec.z/l)}
 }
 
-func dotProduct(a,b Vector) float64{
+func(a Vector) dotProduct(b Vector) float64{
 		return (a.x * b.x) + (a.y * b.y)
 }	
 
-func dotProduct3D(a,b Vector3D) float64{
+func(a Vector3D) dotProduct(b Vector3D) float64{
 		return (a.x * b.x) + (a.y * b.y) + (a.z * b.z)
 }
 
@@ -76,8 +89,28 @@ func(vec Vector) scalarMult(x float64) Vector{
 		return Vector{(vec.x*x),(vec.y*x)}
 }
 
-func(vec Vector3D) scalarMult(x float64) Vector3D{
+func(vec Vector3D) scalarMult(x float64) Vector3D{ 
 		return Vector3D{Vector{(vec.x*x),(vec.y*x)},(vec.z*x)}
+}
+
+func(a Vector) crossProduct(b Vector) float64{		
+		x := (a.x*b.y)-(a.y*b.x)	
+		
+		return x
+}	
+
+func(a Vector3D) crossProduct(b Vector3D) Vector3D{
+		x := (a.y*b.z)-(a.z*b.y)
+		y := (a.z*b.x)-(a.x*b.z)
+		z := (a.x*b.y)-(a.y*b.x)
+	
+		return Vector3D{Vector{x,y},z}
+}
+
+func(a Vector3D) angle(b Vector3D) float64{
+		angle := (a.dotProduct(b))/((a.length())*(b.length()))
+		
+		return math.Acos(angle)*180/math.Pi
 }
 
 /*
@@ -85,6 +118,5 @@ func(vec Vector3D) scalarMult(x float64) Vector3D{
  * into a zero vector
 func(v *Vector) test(){
 		*v = Vector{0,0}
-	
 }	
 */
